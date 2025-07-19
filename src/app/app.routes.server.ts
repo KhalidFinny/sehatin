1a19181f80c645e8c47a4c29ee26cfd866cfee7f
a6iyyu <mizukinako7@gmail.com>
2025-07-19 11:41:23 +0700

    :rocket: Ready for production, bismillah.

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,16 @@
 import { RenderMode, ServerRoute } from "@angular/ssr";
+import { getAllDistricts } from "indonesia-nodejs";
 
 export const serverRoutes: ServerRoute[] = [
+  {
+    path: "admin/kecamatan/:id",
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      const allDistricts = await getAllDistricts();
+      const malangDistricts = allDistricts.filter(d => d.city_code === 3507);
+      return malangDistricts.map(d => ({ id: String(d.city_code) }));
+    }
+  },
   {
     path: "**",
     renderMode: RenderMode.Prerender,
