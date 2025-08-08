ddfdecdaa93447392c7908da47f36ab20ef68e39
a6iyyu <mizukinako7@gmail.com>
2025-08-08 18:35:06 +0700

    :wrench: Fix getPrerenderParams for articles page.

--- apps/client/src/app/app.routes.server.ts
+++ apps/client/src/app/app.routes.server.ts
@@ -1,4 +1,5 @@
 import { RenderMode, ServerRoute } from "@angular/ssr";
+import { listOfArticles } from "@constants/list-of-articles";
 import { getAllDistricts } from "indonesia-nodejs";
 
 export const serverRoutes: ServerRoute[] = [
@@ -9,7 +10,14 @@ export const serverRoutes: ServerRoute[] = [
       const allDistricts = await getAllDistricts();
       const malangDistricts = allDistricts.filter(d => d.city_code === 3507);
       return malangDistricts.map(d => ({ id: String(d.city_code) }));
-    }
+    },
+  },
+  {
+    path: "artikel/:slug",
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      return listOfArticles.map(a => ({ slug: a.slug }));
+    },
   },
   {
     path: "**",
